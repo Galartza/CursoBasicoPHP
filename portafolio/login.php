f34d272e8590bdf30a9b391cf3300773ac20cf8b
joa <jotaquin99g@gmail.com>
2023-09-21 16:10:40 -0300

    agregando estilos al form login

--- portafolio/login.php
+++ portafolio/login.php
@@ -1,14 +1,49 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-  </body>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+
+            </div>
+            <div class="col-md-4">
+                <div class="card text-center">
+                    <div class="card-header">
+                        Iniciar sesión
+                    </div>
+                    <div class="card-body">
+
+                        <form action="login.php" method="post">
+                            Usuario: <input class="form-control" type="text" name="usuario" id="">
+                            <br />
+                            Contraseña: <input class="form-control" type="text" name="contrasenia" id="">
+                            <br />
+
+                        </form>
+                    </div>
+                    <div class="card-footer text-body-secondary">
+                        <button class="btn btn-success" type="submit">Entrar al portfolio</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+
+            </div>
+        </div>
+
+    </div>
+
+
+
+</body>
+
 </html>
